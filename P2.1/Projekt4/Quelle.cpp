d57de0c9210e458ef87b72fd019e2e0002f5ee6e
kasimAbi <kasim.mermer94@hotmail.de>
2024-04-23 15:16:35 +0200

    projects upload

---
+++ P2.1/Projekt4/Quelle.cpp
@@ -0,0 +1,38 @@
+
+
+/************************************************/
+/* Programm: P2A1.cpp                           */
+/*                                              */
+/* einfaches Programm zur Ausgabe eines Textes	*/
+/*						                        */
+/* Autor: 	G. Neugebauer			            */
+//Kommentar wurde falsch gesetzt
+/*						                        */
+/* erstellt:	25.12.2000			            */
+/*						                        */
+/* Version:	1				                    */
+/*						                        */
+/* Bemerkung:   Programm mit x Fehlern		    */
+/*						                        */
+/************************************************/
+
+
+/*Fehler war das dort das h gefehlt hat die dreiecksklammern falsch gesetzt wurden*/
+#include<stdio.h>
+
+
+/*Die Fehler waren das anstatt int void geschrieben wurde und das m beim main war groﬂ*/
+int main(void)
+{
+
+	// semikolon fehlt \n wurde falsch gesetzt
+	printf("\t * Dieses Programm gibt einen Text aus *\n");
+	//Damit das Programm anh‰lt
+	getchar();
+	//Return fehlt
+	return (1);
+
+}
+
+// Kommentar falsch gesetzt
+/* Ende Beispielprogramm */
---
+++ P2.2/Projekt4/Quelle.cpp
@@ -0,0 +1,40 @@
+
+/************************************************/
+/* Programm: P2A2.cpp                           */
+/*                                              */
+/* einfaches Programm zur Benutzung des Debugger*/
+/*						                        */
+/* Autor: 	G. Neugebauer			            */
+/*						                        */
+/* erstellt:	25.12.2000			            */
+/*						                        */
+/* Version:	1				                    */
+/*						                        */
+/* Bemerkung:   geschachtelte Schleife		    */
+/*						                        */
+/************************************************/
+
+
+#include<stdio.h>
+
+int main(void)
+{
+	int index_x, index_y, ergebnis;
+	float wert = 0.0;		//Wert 0 zuweisen
+
+	for (index_y = 0; index_y<5; index_y++)
+	{
+		for (index_x = 0; index_x<5; index_x++)
+		{
+			ergebnis = index_x + index_y;
+			printf("\nErste Schleife: %i, Zweite Schleife:  %i", index_y, index_x);
+			printf("\nErgebnis: %i,  Wert: %f\n", ergebnis, wert);
+		}
+	}
+	getchar();
+	return(1);
+}
+
+
+
+/* Ende Beispielprogramm */
